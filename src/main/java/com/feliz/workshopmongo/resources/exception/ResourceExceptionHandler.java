aa22f94a867e8b116495981870cabd8a53ca4b54
matheus-feliz <matheusfoliveira172@gmail.com>
2021-10-01 06:28:50 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/feliz/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.feliz.workshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.feliz.workshopmongo.service.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "não encontrado", e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
--- src/main/java/com/feliz/workshopmongo/service/UserService.java
+++ src/main/java/com/feliz/workshopmongo/service/UserService.java
@@ -1,22 +1,29 @@
 package com.feliz.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.feliz.workshopmongo.domain.User;
 import com.feliz.workshopmongo.repository.UserRepository;
+import com.feliz.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
 	}
 
 }
