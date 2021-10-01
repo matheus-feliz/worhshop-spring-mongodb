517d488acc609a38e04ac820cc78d757aec2ccec
matheus-feliz <matheusfoliveira172@gmail.com>
2021-10-01 17:43:58 -0300

    Atualização de usuário com PUT

--- src/main/java/com/feliz/workshopmongo/service/UserService.java
+++ src/main/java/com/feliz/workshopmongo/service/UserService.java
@@ -36,6 +36,18 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public void updateData(User newobj, User obj ) {
+		newobj.setName(obj.getName());
+		newobj.setEmail(obj.getEmail());
+		
+	}
+	
+	public User update(User obj) {
+		User newobj = findById(obj.getId());
+		updateData(newobj, obj);
+		return repo.save(newobj);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
